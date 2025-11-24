79579bf61cfd4631d8de4bd459ab112ccc7d0146
hurshah7862 <hurshah7862@gmail.com>
2025-11-24 16:03:09 -0700

    env added

---
+++ packages/server/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type{ Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World from the server package!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
